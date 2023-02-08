4a62eff1eeef9d933e9acafefac99ab580595783
Bernd Beuster <bernd.beuster@gmail.com>
2023-02-08 20:58:13 +0100

    Add Verilator simulation

--- env.sh
+++ env.sh
@@ -6,6 +6,7 @@ SIMDIR=simdir
 mkdir -p $BDIR $VDIR $SIMDIR
 
 export BSC_OPTIONS="-p src:test:+ -bdir $BDIR -vdir $VDIR -simdir $SIMDIR -o bsim"
+export BSC_VSIM_FLAGS="--no-timing"
 
 # uncomment for better RTL synthesis results
 #export BSC_OPTIONS="$BSC_OPTIONS -opt-undetermined-vals -unspecified-to X"
